547f066ac7e52304ff6330551395b0460d3a47e6
ad3ller <a.deller@ucl.ac.uk>
2018-05-11 17:37:00 +0100

    add plotting tools

---
+++ e11/plotting.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri May 10 17:14:09 2018
+
+@author: Adam
+
+    misc. plotting tools
+
+"""
+import numpy as np
+
+def floating_xticks(ax, xticks, y_pos, length, **kwargs):
+    """ draw ticks inside ax at y=pos. 
+    """
+    if 'color' not in kwargs:
+        kwargs['color'] = 'k'
+    if 'lw' not in kwargs:
+        kwargs['lw'] = 1.0
+    ax.plot([np.min(xticks), np.max(xticks)], [y_pos, y_pos], **kwargs)
+    for tx in xticks:
+        ax.plot([tx, tx], [y_pos, y_pos + length], **kwargs)
+    return ax
+
+def floating_xlabels(ax, xticks, labels, y_pos, **kwargs):
+    """ draw labels at x=ticks inside ax at y=y_pos.
+    """
+    if 'ha' not in kwargs:
+        kwargs['ha'] = 'center'
+    for tx, lbl in zip(xticks, labels):
+        ax.text(tx, y_pos, str(lbl), **kwargs)
+    return ax
