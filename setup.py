098b7be8c0c6cc52a4157c6912492800e26866b7
ad3ller <a.deller@ucl.ac.uk>
2018-10-04 20:04:11 +0100

    collections.abc fix

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='e11',
-      version='0.0.19',
+      version='0.0.20',
       description='process data from experiments',
       url='',
       author='Adam Deller',
